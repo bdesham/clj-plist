a6185ce192fe79c411965a096b6a7e84e534f5dd
Ben Cook <b@bencook.ca>
2015-11-20 03:23:36 -0800

    Require Clojure 1.5 so duplicate map keys do not throw exceptions.
    JIRA ticket CLJ-1065, fixed in Clojure 1.5, changed the behaviour
    of the hash-map constructor to allow duplicate keys, keeping the value
    of the last one. Ideally a plist <dict> would not have duplicate keys,
    but I've found them in iTune's Library XML, for example.
    
    However, this means that now it's possible to lose data when parsing.
    The alternative would be to allow passing in a merge function to be used
    when the same key appears more than once in a dict.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"
                       :comments "Same as Clojure"}
-            :dependencies [[org.clojure/clojure "1.4.0"]
+            :dependencies [[org.clojure/clojure "1.5.0"]
                            [joda-time "1.6"]
                            [commons-codec/commons-codec "1.4"]])
